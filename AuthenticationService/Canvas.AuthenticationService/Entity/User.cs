142191ecb365c390a16bc4c5fe0b0e3d93e1fd55
Ella Zuidema <ella.zuidema@student.fontys.nl>
2021-09-29 11:47:29 +0200

    Initial start AuthenticationService

---
+++ AuthenticationService/Canvas.AuthenticationService/Entity/User.cs
@@ -0,0 +1,30 @@
+﻿namespace AuthenticationService.Entity
+{
+    using Microsoft.AspNetCore.Identity;
+
+    /// <summary>
+    /// User class.
+    /// </summary>
+    public class User : IdentityUser
+    {
+        /// <summary>
+        /// Gets or sets the foreign key of the record inside the person table that is associated with this user.
+        /// </summary>
+        public int? PersonId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the first name of the account owner.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the document number of the account owner.
+        /// </summary>
+        public string DocumentNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name of the account owner.
+        /// </summary>
+        public string LastName { get; set; }
+    }
+}
