2d61bed27a2381501eebc9bf3ef715f17bfdb8f2
Ella Zuidema <ella.zuidema@student.fontys.nl>
2021-11-23 10:23:52 +0100

    Added event bus class

---
+++ Auth-Service.Web/Logic/EventbusSend.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Auth_Service.Web.Logic
+{
+    public class EventbusSend
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "auth_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                var message = GetMessageCreateUser(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.CorrelationId = Guid.NewGuid().ToString();
+
+                channel.BasicPublish(exchange: "", routingKey: "auth_queue", basicProperties: properties, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessageCreateUser(string[] args)
+        {
+            string message = "test";
+            return ((args.Length > 0) ? string.Join(" ", args) : message);
+        }
+    }
+}
